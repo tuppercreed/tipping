a6dcf4cb3af939932b8b3736c6c53b27da6fb399
confodere <l.tuppercreed@gmail.com>
2022-05-27 21:12:16 +1000

    change redirect to server side

--- src/pages/tip/index.tsx
+++ src/pages/tip/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 import { supabase } from '../../modules/supabase/client';
 
-export async function getStaticProps() {
+export const getServerSideProps: GetServerSideProps = async () => {
     const { data, error } = await supabase.from('next_round').select('round_number');
     if (data !== null) {
-        return { redirect: { destination: `/tip/${data[0]['round_number']}`, permanent: false } }
+        return {
+            redirect: {
+                destination: `/tip/${data[0]['round_number']}`,
+                permanent: false,
+            }
+        }
+    }
+    return {
+        props: {}
     }
 }
 
 
 export default function Tip() {
-    return null;
+    return (
+        <h1 className='text-2xl'>No round found</h1>
+    );
 }
